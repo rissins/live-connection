b9e496d16847e0a8119801c5995ee35a2e22e598
rissins <tnalsqqq@naver.com>
2022-12-29 16:49:45 +0900

    style: 불필요 주석 삭제

--- src/main/java/rissins/liveconnection/config/WebSocketConfig.java
+++ src/main/java/rissins/liveconnection/config/WebSocketConfig.java
@@ -5,21 +5,11 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-//@RequiredArgsConstructor
-//@EnableWebSocket
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     public static int count = 0;
 
-//    private final LiveConnectionHandler liveConnectionHandler;
-
-//    @Override
-//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//
-//        registry.addHandler(liveConnectionHandler, "/ws/chat")
-//                .setAllowedOrigins("*");
-//    }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
@@ -30,7 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/queue", "/topic");
-
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
